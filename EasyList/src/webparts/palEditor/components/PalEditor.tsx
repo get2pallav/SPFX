3c49afa03f3ab2712777dbfe9b30d61cf96f78fe
Pallav Mathur <pmathur@codesigned.com>
2018-03-01 15:22:21 -0500

    update

--- EasyList/src/webparts/palEditor/components/PalEditor.tsx
+++ EasyList/src/webparts/palEditor/components/PalEditor.tsx
@@ -2,24 +2,30 @@ import * as React from 'react';
 import styles from './PalEditor.module.scss';
 import { IPalEditorProps } from './IPalEditorProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+const ClassicEditor = require('@ckeditor/ckeditor5-build-classic');
+import Image from '@ckeditor/ckeditor5-image/src/image';
+
 
 export default class PalEditor extends React.Component<IPalEditorProps, {}> {
   public render(): React.ReactElement<IPalEditorProps> {
     return (
-      <div className={ styles.palEditor }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
+      <div id="palEditor">
       </div>
     );
   }
+
+  public componentDidMount() {
+    this.getCKeditor();
+  }
+
+  private getCKeditor(): void {
+    ClassicEditor.create(document.querySelector("#palEditor"))
+      .then(editor => {
+        console.log(editor);
+      })
+      .error(error => {
+        console.log(error)
+      })
+  }
+
 }
