45e55919c08fb7a29b95e9700c266730198428c5
Pallav Mathur <pmathur@codesigned.com>
2018-03-01 11:39:14 -0500

    Editor webpart

---
+++ EasyList/src/webparts/palEditor/PalEditorWebPart.ts
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'PalEditorWebPartStrings';
+import PalEditor from './components/PalEditor';
+import { IPalEditorProps } from './components/IPalEditorProps';
+
+export interface IPalEditorWebPartProps {
+  description: string;
+}
+
+export default class PalEditorWebPart extends BaseClientSideWebPart<IPalEditorWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IPalEditorProps > = React.createElement(
+      PalEditor,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ EasyList/src/webparts/palEditor/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IPalEditorWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'PalEditorWebPartStrings' {
+  const strings: IPalEditorWebPartStrings;
+  export = strings;
+}
